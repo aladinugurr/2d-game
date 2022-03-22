7dc431eba4483a8f0a45c7a177e46a58e441e1cb
alaaddin <ugur.catal8@gmail.com>
2022-03-23 02:57:43 +0300

    map-added and assets-added

--- Assets/Scripts/dusman.cs
+++ Assets/Scripts/dusman.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class dusman : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject enemy;
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject tesla;
+
     void Start()
     {
       
-        Instantiate(enemy, new Vector2(2f, 0), Quaternion.identity);
+        Instantiate(enemy1, new Vector3(6f, 0,-1f), Quaternion.identity);
+        Instantiate(enemy2, new Vector3(6f, 1f,-1f), Quaternion.identity);
+        Instantiate(tesla, new Vector3(6f, -1f,-1f), Quaternion.identity);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime);
+      
     }
 }
